cf5ac9ce3902716b79ddf4f7568a5dfe4212c9e4
stl008 <stl008@ucsd.edu>
2022-10-30 14:16:24 -0700

    attempt first requirement

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,22 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const horn = document.querySelector("horn-select");
+  if(horn.value == "air-horn")
+  {
+    document.getElementById("expose").src = "air-horn.svg";
+    document.querySelector("hidden").src = "air-horn.mp3";
+  }
+  if(horn.value == "car-horn")
+  {
+    document.getElementById("expose").src = "car-horn.svg";
+    document.querySelector("hidden").src = "car-horn.mp3";
+  }
+  if(horn.value == "party-horn")
+  {
+    document.getElementById("expose").src = "party-horn.svg";
+    document.querySelector("hidden").src = "party-horn.mp3";
+  }
+
+  
 }
