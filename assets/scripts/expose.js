4ed54a29a18c144687af79a48f79bedadeb91b38
stl008 <stl008@ucsd.edu>
2022-10-30 15:18:03 -0700

    edit attempt for first requirement

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,21 +4,23 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   const horn = document.querySelector("select");
-  if(horn.value == "air-horn")
-  {
-    document.querySelector("img").src = "air-horn.svg";
-    document.getElementsByClassName("hidden")[0].src = "air-horn.mp3";
-  }
-  else if(horn.value == "car-horn")
-  {
-    document.querySelector("img").src = "car-horn.svg";
-    document.getElementsByClassName("hidden")[0].src = "car-horn.mp3";
-  }
-  else if(horn.value == "party-horn")
-  {
-    document.querySelectro("img").src = "party-horn.svg";
-    document.getElementsByClassName("hidden")[0].src = "party-horn.mp3";
-  }
+  horn.addEventListener("change", (event) => {
+    if(horn.value == "air-horn")
+    {
+      document.querySelector("img").src = "air-horn.svg";
+      document.getElementsByClassName("hidden")[0].src = "air-horn.mp3";
+    }
+    else if(horn.value == "car-horn")
+    {
+      document.querySelector("img").src = "car-horn.svg";
+      document.getElementsByClassName("hidden")[0].src = "car-horn.mp3";
+    }
+    else if(horn.value == "party-horn")
+    {
+      document.querySelectro("img").src = "party-horn.svg";
+      document.getElementsByClassName("hidden")[0].src = "party-horn.mp3";
+    }
+  });
 
   const vol = document.getElementById("volume-controls");
   if(vol.value == 0)
