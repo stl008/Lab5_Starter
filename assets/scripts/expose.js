2fd8e73220a2810f0971b80e968186ede86cd1cf
stl008 <stl008@ucsd.edu>
2022-10-30 15:42:50 -0700

    edit attempt for first requirement

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -7,7 +7,7 @@ function init() {
   horn.addEventListener("change", (event) => {
     if(horn.value == "air-horn")
     {
-      document.querySelector("img").src = "assets/images/air-horn.svg";
+      document.querySelector("img").src = "images/air-horn.svg";
       document.getElementsByClassName("hidden")[0].src = "assets/audio/air-horn.mp3";
     }
     else if(horn.value == "car-horn")
@@ -26,19 +26,19 @@ function init() {
   vol.addEventListener("input", (event) => {
     if(vol.value == 0)
     {
-      vol.src = "./icons/volume-level-0.svg";
+      vol.src = "assets/icons/volume-level-0.svg";
     }
     else if(vol.value < 33)
     {
-      vol.src = "./icons/volume-level-1.svg";
+      vol.src = "assets/icons/volume-level-1.svg";
     }
     else if (vol.value < 67)
     {
-      vol.src = "./icons/volume-level-2.svg";
+      vol.src = "assets/icons/volume-level-2.svg";
     }
     else
     {
-      volume.src = "./icons/volume-level-3.svg";
+      vol.src = "assets/icons/volume-level-3.svg";
     }
     volume.volume = volume.value/100;
   });
