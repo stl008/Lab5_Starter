4bbd3d82f472257bb84efe29089f53fa034de088
stl008 <stl008@ucsd.edu>
2022-10-30 16:15:41 -0700

    edit attempt for first requirement

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -8,17 +8,17 @@ function init() {
     if(horn.value == "air-horn")
     {
       document.querySelector("img").src = "assets/images/air-horn.svg";
-      document.getElementsByClassName("hidden")[0].src = "assets/audio/air-horn.mp3";
+      document.querySelector("audio").src = "assets/audio/air-horn.mp3";
     }
     else if(horn.value == "car-horn")
     {
       document.querySelector("img").src = "assets/images/car-horn.svg";
-      document.getElementsByClassName("hidden")[0].src = "assets/audio/car-horn.mp3";
+      document.querySelector("audio").src = "assets/audio/car-horn.mp3";
     }
     else if(horn.value == "party-horn")
     {
       document.querySelector("img").src = "assets/images/party-horn.svg";
-      document.getElementsByClassName("hidden")[0].src = "assets/audio/party-horn.mp3";
+      document.querySelector("audio").src = "assets/audio/party-horn.mp3";
     }
   });
 
@@ -26,19 +26,19 @@ function init() {
   vol.addEventListener("input", (event) => {
     if(vol.value == 0)
     {
-      document.querySelector("img").src = "assets/icons/volume-level-0.svg";
+      document.getElementById("volume-controls").querySelector("img").src = "assets/icons/volume-level-0.svg";
     }
     else if(vol.value < 33)
     {
-      document.querySelector("img").src = "assets/icons/volume-level-1.svg";
+      document.getElementById("volume-controls").querySelector("img").src = "assets/icons/volume-level-1.svg";
     }
     else if (vol.value < 67)
     {
-      document.querySelector("img").src = "assets/icons/volume-level-2.svg";
+      document.getElementById("volume-controls").querySelector("img").src = "assets/icons/volume-level-2.svg";
     }
     else
     {
-      document.querySelector("img").src = "assets/icons/volume-level-3.svg";
+      document.getElementById("volume-controls").querySelector("img").src = "assets/icons/volume-level-3.svg";
     }
     vol.volume = (vol.value)/100;
   });
