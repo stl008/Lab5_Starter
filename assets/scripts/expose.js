03ebdbf7b5636c87459454f333bb6075ee8394fd
stl008 <stl008@ucsd.edu>
2022-10-30 15:31:20 -0700

    edit attempt for first requirement

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -7,18 +7,18 @@ function init() {
   horn.addEventListener("change", (event) => {
     if(horn.value == "air-horn")
     {
-      document.querySelector("img").src = "./assets/images/air-horn.svg";
-      document.getElementsByClassName("hidden")[0].src = "./assets/audio/air-horn.mp3";
+      document.querySelector("img").src = "./images/air-horn.svg";
+      document.getElementsByClassName("hidden")[0].src = "./audio/air-horn.mp3";
     }
     else if(horn.value == "car-horn")
     {
-      document.querySelector("img").src = "./assets/images/car-horn.svg";
-      document.getElementsByClassName("hidden")[0].src = "./assets/audio/car-horn.mp3";
+      document.querySelector("img").src = "./images/car-horn.svg";
+      document.getElementsByClassName("hidden")[0].src = "./audio/car-horn.mp3";
     }
     else if(horn.value == "party-horn")
     {
-      document.querySelectro("img").src = "./assets/images/party-horn.svg";
-      document.getElementsByClassName("hidden")[0].src = "./assets/audio/party-horn.mp3";
+      document.querySelectro("img").src = "./images/party-horn.svg";
+      document.getElementsByClassName("hidden")[0].src = "./audio/party-horn.mp3";
     }
   });
 
@@ -26,19 +26,19 @@ function init() {
   vol.addEventListener("input", (event) => {
     if(vol.value == 0)
     {
-      vol.src = "assets/icons/volume-level-0.svg";
+      vol.src = "./icons/volume-level-0.svg";
     }
     else if(vol.value < 33)
     {
-      vol.src = "assets/icons/volume-level-1.svg";
+      vol.src = "./icons/volume-level-1.svg";
     }
     else if (vol.value < 67)
     {
-      vol.src = "assets/icons/volume-level-2.svg";
+      vol.src = "./icons/volume-level-2.svg";
     }
     else
     {
-      volume.src = "assets/icons/volume-level-3.svg";
+      volume.src = "./icons/volume-level-3.svg";
     }
     volume.volume = volume.value/100;
   });
