16ee0588e94dfd1075d2002370aa1a6cb32897d5
stl008 <stl008@ucsd.edu>
2022-10-30 16:07:26 -0700

    edit attempt for first requirement

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -8,7 +8,7 @@ function init() {
     if(horn.value == "air-horn")
     {
       document.querySelector("img").src = "assets/images/air-horn.svg";
-      document.getElementsByClassName("hidden").src = "assets/audio/air-horn.mp3";
+      document.getElementsByClassName("hidden")[0].src = "assets/audio/air-horn.mp3";
     }
     else if(horn.value == "car-horn")
     {
@@ -22,25 +22,25 @@ function init() {
     }
   });
 
-  const vol = document.getElementById("volume-controls");
+  const vol = document.getElementById("volume");
   vol.addEventListener("input", (event) => {
     if(vol.value == 0)
     {
-      vol.src = "assets/icons/volume-level-0.svg";
+      document.querySelector("img").src = "assets/icons/volume-level-0.svg";
     }
     else if(vol.value < 33)
     {
-      vol.src = "assets/icons/volume-level-1.svg";
+      document.querySelector("img").src = "assets/icons/volume-level-1.svg";
     }
     else if (vol.value < 67)
     {
-      vol.src = "assets/icons/volume-level-2.svg";
+      document.querySelector("img").src = "assets/icons/volume-level-2.svg";
     }
     else
     {
-      vol.src = "assets/icons/volume-level-3.svg";
+      document.querySelector("img").src = "assets/icons/volume-level-3.svg";
     }
-    volume.volume = volume.value/100;
+    vol.volume = (vol.value)/100;
   });
   
 }
